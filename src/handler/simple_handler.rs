365a0f738389307b867e1d0bba32ae844a8f4972
zavakid <zava.kid@gmail.com>
2023-01-11 11:23:16 +0800

    refactor: layout response

--- src/handler/simple_handler.rs
+++ src/handler/simple_handler.rs
@@ -3,14 +3,14 @@ use crate::protocol::{Request, Response, StatusCode};
 
 pub struct SimpleHandler {}
 
-impl Handler for SimpleHandler {
-    fn handle(&self, request: &Request) -> Response {
-        Response::new(StatusCode::OK, Some("it works".into()))
-    }
-}
-
 impl SimpleHandler {
     pub fn new() -> Self {
         SimpleHandler {}
+    }
+}
+
+impl Handler for SimpleHandler {
+    fn handle(&self, request: &Request) -> Response {
+        Response::new(StatusCode::OK, Some("it works".into()))
     }
 }
